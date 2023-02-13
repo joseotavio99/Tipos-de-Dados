d6065f48fcabac716f485c59f36c09db66fafaa8
joseotavio99 <joseotavioetec@gmail.com>
2023-02-13 02:42:09 +0000

    Salvar

--- EstruturaDeSelecao.java
+++ EstruturaDeSelecao.java
@@ -1,5 +1,20 @@
 public class EstruturaDeSelecao {
 
+    public static void main(String[] args){
+    
+        int idade;
+
+        idade=19;
+
+        if (idade >= 18)
+        {
+            System.out.print("apto ao Serviço Militar Obrigatório!");
+            
+        } else {
+            System.out.print("Não esta apto ao Serviço Militar Obrigatório!");          
+        }
+
+    }
 
 
 }
