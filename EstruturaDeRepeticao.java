49c3149a5ca369e6d2b56047349fc83fa9b624ae
joseotavio99 <joseotavioetec@gmail.com>
2023-02-13 02:34:37 +0000

    Salvar

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,9 +1,9 @@
 public class EstruturaDeRepeticao {
     public static void main(String[] args){
 
-    int cc = 1;
-        while (cc<=4){
-        System.out.println("teste" + cc);
+    int cc = 0;
+        while (cc<4){
+        System.out.println("teste" + (cc+1));
         cc++;
         }
     }
