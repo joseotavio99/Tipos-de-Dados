cb30fbb18ca50c3586d168d6b52a9d314c8176ff
joseotavio99 <joseotavioetec@gmail.com>
2023-02-14 02:52:46 +0000

    Salvar

---
+++ InserirObjeto.java
@@ -0,0 +1,24 @@
+public class InserirObjeto {
+
+    public static void main(String[] args){
+    int base, altura, area;
+    
+    System.out.println("Informe o valor da base: ");
+    base = input.nextint();
+
+    System.out.println("Informe o valor da altura: ");
+    altura = input.nextint();
+
+    area = base * altura;
+
+    System.out.printf("A area de um retangulo de base = " + base);
+    System.out.printf("e altura de = " + altura);
+    System.out.printf("é = " + area);
+
+
+
+
+
+    }
+
+}
