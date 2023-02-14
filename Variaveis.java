67bf57dcf0c8a16e2716fc7d60a77b6e0e630053
joseotavio99 <joseotavioetec@gmail.com>
2023-02-14 14:52:59 +0000

    Salvar

--- Variaveis.java
+++ Variaveis.java
@@ -1,10 +1,14 @@
 public class Variaveis {
   
     public static void main(String[] args){
-        int x;
+        double x, y, soma;
         x = 100;
+        y = 10;
+        soma = x + y;
 
         System.out.println("Voce tem \n"+ x);
+        System.out.println("Voce tambem tem \n"+ y);
+        System.out.println("A soma deles são \n"+ soma);
 
     }
     
